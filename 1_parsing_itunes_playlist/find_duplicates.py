58e8fadbd8b24664892045c4d874323872b915cd
hschoonjans <helain90@hotmail.com>
2017-04-28 14:17:49 +0200

    Finding duplicates

--- 1_parsing_itunes_playlist/find_duplicates.py
+++ 1_parsing_itunes_playlist/find_duplicates.py
@@ -8,3 +8,32 @@ Created on Thu Apr 27 13:56:36 2017
 import plistlib
 
 #  finding duplicate tracks with the findDuplicates()
+def find_duplicates(file_name):
+    with open(file_name, 'rb') as fp:
+        pl = plistlib.load(fp) # readPlist is deprecated...
+    tracks = pl["Tracks"]
+    track_names = {}
+    duplicates = []
+    
+    for track_id, track in tracks.items():
+        #try:
+        name = track["Name"]
+        duration = track["Total Time"]
+        duration_seconds = duration//1000
+        duration_str = str(duration_seconds)
+        
+        if not name in track_names.keys():
+            track_names[name] = {}
+            track_names[name][duration_str] = 1
+        elif not duration_str in track_names[name].keys():
+            track_names[name][duration_str] = 1
+        else:
+            track_names[name][duration_str] = track_names[name][duration_str] + 1
+            duplicates.append(name)
+        #except:
+        #    pass
+    
+    # prints the names of the duplicate titles.
+    print(set(duplicates))
+    
+find_duplicates("test-data/mymusic.xml")
